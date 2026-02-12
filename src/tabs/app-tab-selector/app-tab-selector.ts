056a1ee8b4a229a2e074ae01a6993ae4c39082b6
Faragó Tamás <ocsi0520@gmail.com>
2026-02-12 13:28:31 +0100

    fix 'app-app' prefix

--- src/tabs/app-tab-selector/app-tab-selector.ts
+++ src/tabs/app-tab-selector/app-tab-selector.ts
@@ -2,11 +2,9 @@ import { Component } from '@angular/core';
 import { MatTab, MatTabGroup } from '@angular/material/tabs'
 
 @Component({
-  selector: 'app-app-tab-selector',
+  selector: 'app-tab-selector',
   imports: [MatTabGroup, MatTab],
   templateUrl: './app-tab-selector.html',
   styleUrl: './app-tab-selector.scss',
 })
-export class AppTabSelector {
-
-}
+export class AppTabSelector {}
