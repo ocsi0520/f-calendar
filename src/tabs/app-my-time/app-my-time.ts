ad88dcaee8e0a9bb65bdcb252520dcf4b83060a1
Faragó Tamás <ocsi0520@gmail.com>
2026-02-12 13:29:17 +0100

    SNAPSHOT: add app-my-time

---
+++ src/tabs/app-my-time/app-my-time.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-time',
+  imports: [],
+  templateUrl: './app-my-time.html',
+  styleUrl: './app-my-time.scss',
+})
+export class AppMyTime {
+
+}
--- src/tabs/app-tab-selector/app-tab-selector.ts
+++ src/tabs/app-tab-selector/app-tab-selector.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { MatTab, MatTabGroup } from '@angular/material/tabs'
+import { MatTab, MatTabGroup } from '@angular/material/tabs';
+import { AppMyTime } from '../app-my-time/app-my-time';
 
 @Component({
   selector: 'app-tab-selector',
-  imports: [MatTabGroup, MatTab],
+  imports: [MatTabGroup, MatTab, AppMyTime],
   templateUrl: './app-tab-selector.html',
   styleUrl: './app-tab-selector.scss',
 })
