990bb52b1f75c7b06bd4373af2dfac8a7f12246f
Faragó Tamás <ocsi0520@gmail.com>
2026-02-13 16:57:36 +0100

    add register new client component

--- src/tabs/app-tab-selector/app-tab-selector.ts
+++ src/tabs/app-tab-selector/app-tab-selector.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatTab, MatTabGroup } from '@angular/material/tabs';
 import { AppMyTime } from '../app-my-time/app-my-time';
+import { RegisterNewClient } from "../register-new-client/register-new-client";
 
 @Component({
   selector: 'app-tab-selector',
-  imports: [MatTabGroup, MatTab, AppMyTime],
+  imports: [MatTabGroup, MatTab, AppMyTime, RegisterNewClient],
   templateUrl: './app-tab-selector.html',
   styleUrl: './app-tab-selector.scss',
 })
---
+++ src/tabs/register-new-client/register-new-client.ts
@@ -0,0 +1,56 @@
+import { Component, inject } from '@angular/core';
+import { ClientService } from '../../client/client.service';
+import { FormBuilder, Validators, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { Client } from '../../client/Client';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-register-new-client',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+  ],
+  templateUrl: './register-new-client.html',
+  styleUrl: './register-new-client.scss',
+})
+export class RegisterNewClient {
+  clientService = inject(ClientService);
+  private fb = inject(FormBuilder);
+
+  form = this.fb.nonNullable.group({
+    name: ['', [Validators.required, Validators.maxLength(100)]],
+    sessionCountsInWeek: [1, [Validators.required, Validators.min(1), Validators.max(7)]],
+    comment: [''],
+  });
+
+  public submit(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    const newClient = this.form.getRawValue() as Omit<Client, 'schedule'>;
+    this.clientService.addClient({ ...newClient, schedule: [] });
+    this.form.reset();
+  }
+
+  // convenience getters for template
+  get name() {
+    return this.form.controls.name;
+  }
+  get sessions() {
+    return this.form.controls.sessionCountsInWeek;
+  }
+  get comment() {
+    return this.form.controls.comment;
+  }
+}
