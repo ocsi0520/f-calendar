fa322f572360def8d0db6ac0868c8bf31511d40a
Faragó Tamás <ocsi0520@gmail.com>
2026-02-13 16:55:37 +0100

    simplify WeekSchedule

--- src/time/Schedule.ts
+++ src/time/Schedule.ts
@@ -1,11 +1,3 @@
 import { TimeInterval } from './TimeInterval';
 
-export type WeekSchedule = {
-  monday: Array<TimeInterval>;
-  tuesday: Array<TimeInterval>;
-  wednesday: Array<TimeInterval>;
-  thursday: Array<TimeInterval>;
-  friday: Array<TimeInterval>;
-  saturday: Array<TimeInterval>;
-  sunday: Array<TimeInterval>;
-};
+export type WeekSchedule = Array<TimeInterval>;
