a141b0a83a432878ef18ec2d7c91fa97316e8fbb
Faragó Tamás <ocsi0520@gmail.com>
2026-02-18 22:32:38 +0100

    add MorningChecker helper class

---
+++ src/time/ScheduleGenerator/specification/MorningChecker.ts
@@ -0,0 +1,12 @@
+import { TimeManager } from '../../TimeManager';
+import { ScheduleItem } from '../Table';
+
+export class MorningChecker {
+  constructor(private readonly timeMapper: TimeManager) {}
+  // this means, she'll eat breakfast later
+  public isMorningSession(scheduleItem: ScheduleItem): boolean {
+    const startOfFirstSession = this.timeMapper.timeToNumber(scheduleItem.timeInterval.start);
+    const hardLineForBreakfast = this.timeMapper.timeToNumber([8, 45]);
+    return startOfFirstSession <= hardLineForBreakfast;
+  }
+}
---
+++ src/time/ScheduleGenerator/specification/__tests__/MorningChecker.spec.ts
@@ -0,0 +1,34 @@
+import { TimeManager } from '../../../TimeManager';
+import { MorningChecker } from '../MorningChecker';
+import { ScheduleItem } from '../../Table';
+import { DayNumber } from '../../../TimeInterval/TimeInterval-constants';
+
+describe(MorningChecker.name, () => {
+  let timeManager: TimeManager;
+  let unitUnderTest: MorningChecker;
+
+  beforeEach(() => {
+    timeManager = new TimeManager();
+    unitUnderTest = new MorningChecker(timeManager);
+  });
+
+  const makeItem = (day: DayNumber, start: [number, number], end: [number, number]): ScheduleItem => ({
+    timeInterval: { dayNumber: day, start: start as any, end: end as any },
+    clientIdsInvolved: [1],
+  });
+
+  it('returns true for sessions starting at or before 08:45', () => {
+    const item = makeItem(1, [8, 45], [9, 30]);
+    expect(unitUnderTest.isMorningSession(item)).toBe(true);
+  });
+
+  it('returns false for sessions starting after 08:45', () => {
+    const item = makeItem(1, [8, 46], [9, 30]);
+    expect(unitUnderTest.isMorningSession(item)).toBe(false);
+  });
+
+  it('returns true for early morning sessions', () => {
+    const item = makeItem(1, [7, 0], [8, 0]);
+    expect(unitUnderTest.isMorningSession(item)).toBe(true);
+  });
+});
