9049b31c7c499ed08f684dfba31cd9c1d6543b11
Faragó Tamás <ocsi0520@gmail.com>
2026-02-26 14:40:51 +0100

    use timeIntervalManager.isIntervalWithinSchedule in AvailableSpecification

--- src/time/ScheduleGenerator/specification/rules/AvailableSpecification.ts
+++ src/time/ScheduleGenerator/specification/rules/AvailableSpecification.ts
@@ -1,4 +1,3 @@
-import { TimeInterval } from '../../../TimeInterval/TimeInterval';
 import { WeekSchedule } from '../../../Schedule';
 import { ScheduleItem, Table } from '../../Table';
 import { ScheduleSpecification } from '../specification';
@@ -8,14 +7,10 @@ export abstract class AvailableSpecification implements ScheduleSpecification {
   constructor(private readonly timeIntervalManager: TimeIntervalManager) {}
   public abstract check(table: Table): boolean;
 
-  // TODO: use this.timeIntervalManager.isIntervalWithinSchedule()
   protected withinSchedule(item: ScheduleItem, schedule: WeekSchedule): boolean {
-    return schedule.some((timeInterval) => this.itemWithinInterval(item, timeInterval));
+    return this.timeIntervalManager.isIntervalWithinSchedule(item.timeInterval, schedule)
   }
-
-  private itemWithinInterval(item: ScheduleItem, timeInterval: TimeInterval): boolean {
-    return this.timeIntervalManager.doesFirstIncludeSecond(timeInterval, item.timeInterval);
-  }
+  
   protected getAllOccupiedCells(table: Table): Array<ScheduleItem> {
     return table.scheduleItems.filter((item) => item.clientIdsInvolved.length > 0);
   }
