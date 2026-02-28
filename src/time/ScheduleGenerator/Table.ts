e0e227e6f1064904bae1541195c8f0c24babaab4
Faragó Tamás <ocsi0520@gmail.com>
2026-02-28 14:34:30 +0100

    SNAPSHOT: enrich ClientInfo w/ possible cells and unique days

--- src/time/ScheduleGenerator/Table.ts
+++ src/time/ScheduleGenerator/Table.ts
@@ -1,5 +1,8 @@
 import { Client } from '../../client/Client';
 import { TimeInterval } from '../TimeInterval/TimeInterval';
+import { DayNumber } from '../TimeInterval/TimeInterval-constants';
+
+// TODO: type ArrayWithindex = { items: Array<number>; currentIndex: number; }
 
 export type ScheduleCell = {
   timeInterval: TimeInterval;
@@ -8,7 +11,14 @@ export type ScheduleCell = {
 
 export type ClientInfo = {
   client: Client;
-  joinedAt: number[]; // the indexes within ScheduleCells
+  joinedAt: Array<number>; // the indexes within ScheduleCells
+  possibleCellIndexes: Array<number>; // the indexes within ScheduleCells
+  currentIndexOfPossibleCells: number;
+  /**
+   * The number of days where the client has suitable time with trainer.
+   * I.e. 1T7.30_-_10.00, 1T18.30_-_20.00, 3T10.10_-_15.00, 3T19.45_-_21.00, --> 2 (Monday & Wednesday)
+   */
+  uniqueDays: DayNumber;
 };
 
 export type Table = {
