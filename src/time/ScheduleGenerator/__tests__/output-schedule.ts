35d0a0aaa17fb3091a0575b2b511e025bcf99f9f
Faragó Tamás <ocsi0520@gmail.com>
2026-03-02 17:57:56 +0100

    add integration test for ScheduleGenerator

---
+++ src/time/ScheduleGenerator/__tests__/output-schedule.ts
@@ -0,0 +1,199 @@
+import { DisplayableSchedule } from '../../Schedule';
+
+export const expectedBroaderSchedule: DisplayableSchedule = [
+  {
+    dayNumber: 1,
+    start: [10, 45],
+    end: [12, 0],
+    displayName: 'Horváth Zsuzsanna',
+  },
+  {
+    dayNumber: 1,
+    start: [12, 15],
+    end: [13, 30],
+    displayName: 'Szabó Ágoston',
+  },
+  {
+    dayNumber: 1,
+    start: [16, 0],
+    end: [17, 15],
+    displayName: 'Vadai Ágnes',
+  },
+  {
+    dayNumber: 1,
+    start: [17, 15],
+    end: [18, 30],
+    displayName: 'Balogh Attiláné',
+  },
+  {
+    dayNumber: 2,
+    start: [7, 30],
+    end: [8, 45],
+    displayName: 'Balogh Béla',
+  },
+  {
+    dayNumber: 2,
+    start: [9, 30],
+    end: [10, 45],
+    displayName: 'Horváth Zsuzsanna',
+  },
+  {
+    dayNumber: 2,
+    start: [10, 45],
+    end: [12, 0],
+    displayName: 'Gombos Edina',
+  },
+  {
+    dayNumber: 2,
+    start: [12, 15],
+    end: [13, 30],
+    displayName: 'Szabó Ágoston',
+  },
+  {
+    dayNumber: 2,
+    start: [14, 30],
+    end: [15, 45],
+    displayName: 'Suszter Jolán',
+  },
+  {
+    dayNumber: 2,
+    start: [15, 45],
+    end: [17, 0],
+    displayName: 'Kovács Anikó',
+  },
+  {
+    dayNumber: 2,
+    start: [19, 0],
+    end: [20, 15],
+    displayName: 'Balogh Attiláné',
+  },
+  {
+    dayNumber: 3,
+    start: [18, 30],
+    end: [19, 45],
+    displayName: 'Kovács Anikó',
+  },
+  {
+    dayNumber: 4,
+    start: [10, 30],
+    end: [11, 45],
+    displayName: 'Szabó Ágoston + Pankotai Lili',
+  },
+  {
+    dayNumber: 4,
+    start: [12, 30],
+    end: [13, 45],
+    displayName: 'Vadai Ágnes',
+  },
+  {
+    dayNumber: 4,
+    start: [17, 15],
+    end: [18, 30],
+    displayName: 'Liptai Klaudia',
+  },
+  {
+    dayNumber: 5,
+    start: [11, 0],
+    end: [12, 15],
+    displayName: 'Suszter Jolán',
+  },
+  {
+    dayNumber: 5,
+    start: [12, 15],
+    end: [13, 30],
+    displayName: 'Pankotai Lili',
+  },
+];
+
+export const expectedSmallerSchedule: DisplayableSchedule = [
+  {
+    dayNumber: 1,
+    start: [12, 15],
+    end: [13, 30],
+    displayName: 'Szabó Ágoston',
+  },
+  {
+    dayNumber: 1,
+    start: [16, 0],
+    end: [17, 15],
+    displayName: 'Vadai Ágnes',
+  },
+  {
+    dayNumber: 1,
+    start: [17, 15],
+    end: [18, 30],
+    displayName: 'Balogh Attiláné',
+  },
+  {
+    dayNumber: 2,
+    start: [7, 30],
+    end: [8, 45],
+    displayName: 'Balogh Béla',
+  },
+  {
+    dayNumber: 2,
+    start: [9, 30],
+    end: [10, 45],
+    displayName: 'Gombos Edina',
+  },
+  {
+    dayNumber: 2,
+    start: [12, 15],
+    end: [13, 30],
+    displayName: 'Szabó Ágoston',
+  },
+  {
+    dayNumber: 2,
+    start: [14, 30],
+    end: [15, 45],
+    displayName: 'Suszter Jolán',
+  },
+  {
+    dayNumber: 2,
+    start: [15, 45],
+    end: [17, 0],
+    displayName: 'Kovács Anikó',
+  },
+  {
+    dayNumber: 2,
+    start: [19, 0],
+    end: [20, 15],
+    displayName: 'Balogh Attiláné',
+  },
+  {
+    dayNumber: 3,
+    start: [18, 30],
+    end: [19, 45],
+    displayName: 'Kovács Anikó',
+  },
+  {
+    dayNumber: 4,
+    start: [10, 30],
+    end: [11, 45],
+    displayName: 'Szabó Ágoston + Pankotai Lili',
+  },
+  {
+    dayNumber: 4,
+    start: [12, 30],
+    end: [13, 45],
+    displayName: 'Vadai Ágnes',
+  },
+  {
+    dayNumber: 4,
+    start: [17, 15],
+    end: [18, 30],
+    displayName: 'Liptai Klaudia',
+  },
+  {
+    dayNumber: 5,
+    start: [11, 0],
+    end: [12, 15],
+    displayName: 'Suszter Jolán',
+  },
+  {
+    dayNumber: 5,
+    start: [12, 15],
+    end: [13, 30],
+    displayName: 'Pankotai Lili',
+  },
+];
