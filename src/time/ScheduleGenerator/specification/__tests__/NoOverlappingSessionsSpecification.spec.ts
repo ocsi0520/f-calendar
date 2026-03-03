3f6c992cf547c9b96f0690ca013f98a13ff775ea
Faragó Tamás <ocsi0520@gmail.com>
2026-03-03 11:35:10 +0100

    add extra tests for NoOverlappingSessionsSpecification

--- src/time/ScheduleGenerator/specification/__tests__/NoOverlappingSessionsSpecification.spec.ts
+++ src/time/ScheduleGenerator/specification/__tests__/NoOverlappingSessionsSpecification.spec.ts
@@ -6,6 +6,7 @@ import { createExpectedResult, makeTable, selectForSpec } from './SpecificationT
 import { TimeManager } from '../../../TimeManager';
 import { Table } from '../../Table';
 import { Result } from '../specification';
+import { TimeInterval } from '../../../TimeInterval/TimeInterval';
 
 describe(methodName(NoOverlappingSessionsSpecification, 'check'), () => {
   let unitUnderTest: NoOverlappingSessionsSpecification;
@@ -25,6 +26,14 @@ describe(methodName(NoOverlappingSessionsSpecification, 'check'), () => {
         table.scheduleCells[table.currentScheduleCellIndex].clientIdsInvolved.length === 0;
       if (isEmptyCell) continue;
       expect(unitUnderTest.check(...selectForSpec(table))).toEqual(createExpectedResult(true));
+    }
+  };
+
+  const testDetailed = (table: Table, expectedResults: Array<Result | null>): void => {
+    for (let i = 0; i < expectedResults.length; i++) {
+      if (expectedResults[i] === null) continue;
+      table.currentScheduleCellIndex = i;
+      expect(unitUnderTest.check(...selectForSpec(table))).toEqual(expectedResults[i]);
     }
   };
 
@@ -195,11 +204,7 @@ describe(methodName(NoOverlappingSessionsSpecification, 'check'), () => {
       createExpectedResult({ dayNumber: 1, start: [12, 15], end: [13, 30] }),
       null,
     ];
-    for (let i = 0; i < expectedResults.length; i++) {
-      if (expectedResults[i] === null) continue;
-      table.currentScheduleCellIndex = i;
-      expect(unitUnderTest.check(...selectForSpec(table))).toEqual(expectedResults[i]);
-    }
+    testDetailed(table, expectedResults);
   });
 
   it('returns true for all occupied cells (mixed with non-occupied cells)', () => {
@@ -226,36 +231,7 @@ describe(methodName(NoOverlappingSessionsSpecification, 'check'), () => {
       { timeInterval: { dayNumber: 1, start: [12, 15], end: [13, 30] }, clientIdsInvolved: [] },
       { timeInterval: { dayNumber: 1, start: [12, 30], end: [13, 45] }, clientIdsInvolved: [6] },
     ]);
-    const expectedResults: Array<Result | null> = [
-      /*07:30*/ createExpectedResult(true),
-      /*07:45*/ null,
-      /*08:00*/ null,
-      /*08:15*/ null,
-      /*08:30*/ null,
-      /*08:45*/ createExpectedResult(true),
-      /*09:00*/ null,
-      /*09:15*/ null,
-      /*09:30*/ null,
-      /*09:45*/ null,
-      /*10:00*/ createExpectedResult(true),
-      /*10:15*/ null,
-      /*10:30*/ null,
-      /*10:45*/ null,
-      /*11:00*/ null,
-      /*11:15*/ createExpectedResult(true),
-      /*11:30*/ null,
-      /*11:45*/ null,
-      /*12:00*/ null,
-      /*12:15*/ null,
-      /*12:30*/ createExpectedResult(true),
-    ];
-    for (let i = 0; i < expectedResults.length; i++) {
-      if (expectedResults[i] === null) continue;
-      table.currentScheduleCellIndex = i;
-      expect(unitUnderTest.check(...selectForSpec(table)), `wrong at ${i}`).toEqual(
-        expectedResults[i],
-      );
-    }
+    acceptAllNonEmpty(table);
   });
 
   it('returns true for all occupied cells (mixed with non-occupied cells)', () => {
@@ -266,19 +242,219 @@ describe(methodName(NoOverlappingSessionsSpecification, 'check'), () => {
       { timeInterval: { dayNumber: 1, start: [11, 15], end: [12, 30] }, clientIdsInvolved: [5] },
       { timeInterval: { dayNumber: 1, start: [12, 30], end: [13, 45] }, clientIdsInvolved: [6] },
     ]);
-    const expectedResults: Array<Result | null> = [
-      /*07:30*/ createExpectedResult(true),
-      /*08:45*/ createExpectedResult(true),
-      /*10:00*/ createExpectedResult(true),
-      /*11:15*/ createExpectedResult(true),
-      /*12:30*/ createExpectedResult(true),
-    ];
-    for (let i = 0; i < expectedResults.length; i++) {
-      if (expectedResults[i] === null) continue;
-      table.currentScheduleCellIndex = i;
-      expect(unitUnderTest.check(...selectForSpec(table)), `wrong at ${i}`).toEqual(
-        expectedResults[i],
-      );
-    }
+    acceptAllNonEmpty(table);
+  });
+
+  describe('first-middle-last', () => {
+    describe('scenario 1: initially overlapping with both', () => {
+      it('return false, for the middle due to surroundings', () => {
+        const table = makeTable([
+          { timeInterval: { dayNumber: 1, start: [7, 30], end: [8, 45] }, clientIdsInvolved: [1] },
+          { timeInterval: { dayNumber: 1, start: [8, 15], end: [9, 30] }, clientIdsInvolved: [2] },
+          { timeInterval: { dayNumber: 1, start: [9, 15], end: [10, 30] }, clientIdsInvolved: [3] },
+        ]);
+
+        table.currentScheduleCellIndex = 1;
+        const expectedResult = createExpectedResult({ dayNumber: 1, start: [8, 45], end: [10, 0] });
+        expect(unitUnderTest.check(...selectForSpec(table))).toEqual(expectedResult);
+      });
+
+      it('returns false for middle due to latter one', () => {
+        const table = makeTable([
+          { timeInterval: { dayNumber: 1, start: [7, 30], end: [8, 45] }, clientIdsInvolved: [1] },
+          { timeInterval: { dayNumber: 1, start: [8, 45], end: [10, 0] }, clientIdsInvolved: [2] },
+          { timeInterval: { dayNumber: 1, start: [9, 15], end: [10, 30] }, clientIdsInvolved: [3] },
+        ]);
+
+        table.currentScheduleCellIndex = 1;
+        const expectedInterval: TimeInterval = { dayNumber: 1, start: [10, 30], end: [11, 45] };
+        const expectedResult = createExpectedResult(expectedInterval);
+        expect(unitUnderTest.check(...selectForSpec(table))).toEqual(expectedResult);
+      });
+
+      it('returns true for last', () => {
+        const table = makeTable([
+          { timeInterval: { dayNumber: 1, start: [7, 30], end: [8, 45] }, clientIdsInvolved: [1] },
+          { timeInterval: { dayNumber: 1, start: [9, 15], end: [10, 30] }, clientIdsInvolved: [3] },
+          {
+            timeInterval: { dayNumber: 1, start: [10, 30], end: [11, 45] },
+            clientIdsInvolved: [2],
+          },
+        ]);
+
+        table.currentScheduleCellIndex = 2;
+        const expectedResult = createExpectedResult(true);
+        expect(unitUnderTest.check(...selectForSpec(table))).toEqual(expectedResult);
+      });
+    });
+
+    describe('scenario 2: initially overlapping with first one', () => {
+      it('returns false for middle due to first one', () => {
+        const table = makeTable([
+          { timeInterval: { dayNumber: 1, start: [7, 30], end: [8, 45] }, clientIdsInvolved: [1] },
+          { timeInterval: { dayNumber: 1, start: [8, 0], end: [9, 45] }, clientIdsInvolved: [2] },
+          { timeInterval: { dayNumber: 1, start: [9, 30], end: [10, 45] }, clientIdsInvolved: [3] },
+        ]);
+
+        table.currentScheduleCellIndex = 1;
+        const expectedResult = createExpectedResult({ dayNumber: 1, start: [8, 45], end: [10, 0] });
+        expect(unitUnderTest.check(...selectForSpec(table))).toEqual(expectedResult);
+      });
+
+      it('returns false for middle due to first one', () => {
+        const table = makeTable([
+          { timeInterval: { dayNumber: 1, start: [7, 30], end: [8, 45] }, clientIdsInvolved: [1] },
+          { timeInterval: { dayNumber: 1, start: [8, 45], end: [10, 0] }, clientIdsInvolved: [2] },
+          { timeInterval: { dayNumber: 1, start: [9, 30], end: [10, 45] }, clientIdsInvolved: [3] },
+        ]);
+
+        table.currentScheduleCellIndex = 1;
+        const expectedInterval: TimeInterval = { dayNumber: 1, start: [10, 45], end: [12, 0] };
+        const expectedResult = createExpectedResult(expectedInterval);
+        expect(unitUnderTest.check(...selectForSpec(table))).toEqual(expectedResult);
+      });
+
+      it('returns true', () => {
+        const table = makeTable([
+          { timeInterval: { dayNumber: 1, start: [7, 30], end: [8, 45] }, clientIdsInvolved: [1] },
+          { timeInterval: { dayNumber: 1, start: [9, 30], end: [10, 45] }, clientIdsInvolved: [3] },
+          { timeInterval: { dayNumber: 1, start: [10, 45], end: [12, 0] }, clientIdsInvolved: [2] },
+        ]);
+
+        table.currentScheduleCellIndex = 2;
+        const expectedResult = createExpectedResult(true);
+        expect(unitUnderTest.check(...selectForSpec(table))).toEqual(expectedResult);
+      });
+    });
+
+    describe('scenario 3: initially overlapping with latter one', () => {
+      it('returns false for middle due to latter one', () => {
+        const table = makeTable([
+          { timeInterval: { dayNumber: 1, start: [7, 30], end: [8, 45] }, clientIdsInvolved: [1] },
+          { timeInterval: { dayNumber: 1, start: [9, 0], end: [10, 15] }, clientIdsInvolved: [2] },
+          { timeInterval: { dayNumber: 1, start: [9, 30], end: [10, 45] }, clientIdsInvolved: [3] },
+        ]);
+
+        table.currentScheduleCellIndex = 1;
+        const expectedInterval: TimeInterval = { dayNumber: 1, start: [10, 45], end: [12, 0] };
+        const expectedResult = createExpectedResult(expectedInterval);
+        expect(unitUnderTest.check(...selectForSpec(table))).toEqual(expectedResult);
+      });
+      it('returns true', () => {
+        const table = makeTable([
+          { timeInterval: { dayNumber: 1, start: [7, 30], end: [8, 45] }, clientIdsInvolved: [1] },
+          { timeInterval: { dayNumber: 1, start: [9, 30], end: [10, 45] }, clientIdsInvolved: [3] },
+          { timeInterval: { dayNumber: 1, start: [10, 45], end: [12, 0] }, clientIdsInvolved: [2] },
+        ]);
+
+        table.currentScheduleCellIndex = 2;
+        const expectedResult = createExpectedResult(true);
+        expect(unitUnderTest.check(...selectForSpec(table))).toEqual(expectedResult);
+      });
+    });
+  });
+
+  it('returns false for all, except last one', () => {
+    const table = makeTable([
+      { timeInterval: { dayNumber: 1, start: [8, 0], end: [9, 15] }, clientIdsInvolved: [1] },
+      { timeInterval: { dayNumber: 1, start: [8, 15], end: [9, 30] }, clientIdsInvolved: [2] },
+      { timeInterval: { dayNumber: 1, start: [8, 30], end: [9, 45] }, clientIdsInvolved: [3] },
+      { timeInterval: { dayNumber: 1, start: [8, 45], end: [10, 0] }, clientIdsInvolved: [4] },
+      { timeInterval: { dayNumber: 1, start: [9, 0], end: [10, 15] }, clientIdsInvolved: [5] },
+      { timeInterval: { dayNumber: 1, start: [9, 15], end: [10, 30] }, clientIdsInvolved: [6] },
+      { timeInterval: { dayNumber: 1, start: [9, 30], end: [10, 45] }, clientIdsInvolved: [7] },
+      { timeInterval: { dayNumber: 1, start: [9, 45], end: [11, 0] }, clientIdsInvolved: [8] },
+      { timeInterval: { dayNumber: 1, start: [10, 0], end: [11, 15] }, clientIdsInvolved: [9] },
+      // all of them were before
+      { timeInterval: { dayNumber: 1, start: [10, 15], end: [11, 30] }, clientIdsInvolved: [] },
+      { timeInterval: { dayNumber: 1, start: [10, 30], end: [11, 45] }, clientIdsInvolved: [] },
+      { timeInterval: { dayNumber: 1, start: [10, 45], end: [12, 0] }, clientIdsInvolved: [] },
+      { timeInterval: { dayNumber: 1, start: [11, 0], end: [12, 15] }, clientIdsInvolved: [] },
+      { timeInterval: { dayNumber: 1, start: [11, 15], end: [12, 30] }, clientIdsInvolved: [10] },
+      { timeInterval: { dayNumber: 1, start: [11, 30], end: [12, 45] }, clientIdsInvolved: [] },
+      { timeInterval: { dayNumber: 1, start: [11, 45], end: [13, 0] }, clientIdsInvolved: [] },
+      { timeInterval: { dayNumber: 1, start: [12, 0], end: [13, 15] }, clientIdsInvolved: [] },
+      { timeInterval: { dayNumber: 1, start: [12, 15], end: [13, 30] }, clientIdsInvolved: [11] },
+      { timeInterval: { dayNumber: 1, start: [12, 30], end: [13, 45] }, clientIdsInvolved: [] },
+      { timeInterval: { dayNumber: 1, start: [12, 45], end: [14, 0] }, clientIdsInvolved: [] },
+      { timeInterval: { dayNumber: 1, start: [13, 0], end: [14, 15] }, clientIdsInvolved: [] },
+      { timeInterval: { dayNumber: 1, start: [13, 15], end: [14, 30] }, clientIdsInvolved: [] },
+      { timeInterval: { dayNumber: 1, start: [13, 30], end: [14, 45] }, clientIdsInvolved: [12] },
+      { timeInterval: { dayNumber: 1, start: [13, 45], end: [15, 0] }, clientIdsInvolved: [] },
+      { timeInterval: { dayNumber: 1, start: [14, 0], end: [15, 15] }, clientIdsInvolved: [] },
+      { timeInterval: { dayNumber: 1, start: [14, 15], end: [15, 30] }, clientIdsInvolved: [13] },
+      { timeInterval: { dayNumber: 1, start: [14, 30], end: [15, 45] }, clientIdsInvolved: [] },
+      { timeInterval: { dayNumber: 1, start: [14, 45], end: [16, 0] }, clientIdsInvolved: [] },
+      { timeInterval: { dayNumber: 1, start: [15, 0], end: [16, 15] }, clientIdsInvolved: [] },
+      { timeInterval: { dayNumber: 1, start: [15, 15], end: [16, 30] }, clientIdsInvolved: [] },
+      { timeInterval: { dayNumber: 1, start: [15, 30], end: [16, 45] }, clientIdsInvolved: [14] },
+      { timeInterval: { dayNumber: 1, start: [15, 45], end: [17, 0] }, clientIdsInvolved: [] },
+      { timeInterval: { dayNumber: 1, start: [16, 0], end: [17, 15] }, clientIdsInvolved: [15] },
+      { timeInterval: { dayNumber: 1, start: [16, 15], end: [17, 30] }, clientIdsInvolved: [] },
+      { timeInterval: { dayNumber: 1, start: [16, 30], end: [17, 45] }, clientIdsInvolved: [] },
+      { timeInterval: { dayNumber: 1, start: [16, 45], end: [18, 0] }, clientIdsInvolved: [] },
+      { timeInterval: { dayNumber: 1, start: [17, 0], end: [18, 15] }, clientIdsInvolved: [] },
+      { timeInterval: { dayNumber: 1, start: [17, 15], end: [18, 30] }, clientIdsInvolved: [16] },
+      { timeInterval: { dayNumber: 1, start: [17, 30], end: [18, 45] }, clientIdsInvolved: [17] },
+      { timeInterval: { dayNumber: 1, start: [17, 45], end: [19, 0] }, clientIdsInvolved: [] },
+      { timeInterval: { dayNumber: 1, start: [18, 0], end: [19, 15] }, clientIdsInvolved: [] },
+      { timeInterval: { dayNumber: 1, start: [18, 15], end: [19, 30] }, clientIdsInvolved: [] },
+      { timeInterval: { dayNumber: 1, start: [18, 30], end: [19, 45] }, clientIdsInvolved: [] },
+      { timeInterval: { dayNumber: 1, start: [18, 45], end: [20, 0] }, clientIdsInvolved: [18] },
+      { timeInterval: { dayNumber: 1, start: [19, 0], end: [20, 15] }, clientIdsInvolved: [] },
+      { timeInterval: { dayNumber: 1, start: [19, 15], end: [20, 30] }, clientIdsInvolved: [] },
+      { timeInterval: { dayNumber: 1, start: [19, 30], end: [20, 45] }, clientIdsInvolved: [] },
+      { timeInterval: { dayNumber: 1, start: [19, 45], end: [21, 0] }, clientIdsInvolved: [] },
+    ]);
+    testDetailed(table, [
+      createExpectedResult({ dayNumber: 1, start: [9, 30], end: [10, 45] }),
+      createExpectedResult({ dayNumber: 1, start: [9, 15], end: [10, 30] }),
+      createExpectedResult({ dayNumber: 1, start: [9, 15], end: [10, 30] }),
+      createExpectedResult({ dayNumber: 1, start: [9, 15], end: [10, 30] }),
+      createExpectedResult({ dayNumber: 1, start: [9, 15], end: [10, 30] }),
+      createExpectedResult({ dayNumber: 1, start: [9, 30], end: [10, 45] }),
+      createExpectedResult({ dayNumber: 1, start: [9, 45], end: [11, 0] }),
+      createExpectedResult({ dayNumber: 1, start: [10, 0], end: [11, 15] }),
+      createExpectedResult({ dayNumber: 1, start: [10, 15], end: [11, 30] }),
+      null,
+      null,
+      null,
+      null,
+      createExpectedResult({ dayNumber: 1, start: [13, 30], end: [14, 45] }),
+      null,
+      null,
+      null,
+      createExpectedResult({ dayNumber: 1, start: [12, 30], end: [13, 45] }),
+      null,
+      null,
+      null,
+      null,
+      createExpectedResult({ dayNumber: 1, start: [15, 30], end: [16, 45] }),
+      null,
+      null,
+      createExpectedResult({ dayNumber: 1, start: [14, 45], end: [16, 0] }),
+      null,
+      null,
+      null,
+      null,
+      createExpectedResult({ dayNumber: 1, start: [17, 15], end: [18, 30] }),
+      null,
+      createExpectedResult({ dayNumber: 1, start: [16, 45], end: [18, 0] }),
+      null,
+      null,
+      null,
+      null,
+      createExpectedResult({ dayNumber: 1, start: [18, 45], end: [20, 0] }),
+      createExpectedResult({ dayNumber: 1, start: [18, 30], end: [19, 45] }),
+      null,
+      null,
+      null,
+      null,
+      createExpectedResult(true),
+      null,
+      null,
+      null,
+      null,
+    ]);
   });
 });
