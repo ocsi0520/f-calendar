384bc21108241fffcf34c8ff2d258995f9723a0e
Faragó Tamás <ocsi0520@gmail.com>
2026-03-04 23:47:23 +0100

    use TimeMapper in SameDayIntervalMapper for string representations

--- src/time-management/mappers/SameDayIntervalMapper.ts
+++ src/time-management/mappers/SameDayIntervalMapper.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
-import { DayTime, SameDayInterval } from '../definition/TimeInterval';
+import { SameDayInterval } from '../definition/TimeInterval';
 import { TimeMapper } from './TimeMapper';
-import { DayNumber, Hour, Minute } from '../definition/time-components';
 
-const TIME_REGEX: RegExp = /^([01]\d|2[0-3]):([0-5]\d)$/;
 const TIME_DIVIDER = '_-_';
 
 const NUMBER_BASE = 7 * 24 * 60;
@@ -43,7 +41,11 @@ export class SameDayIntervalMapper {
   }
 
   public mapToString(SameDayInterval: SameDayInterval): string {
-    return `${SameDayInterval.dayNumber}T${this.timeToString(SameDayInterval.start)}_-_${this.timeToString(SameDayInterval.end)}`;
+    const timePart = [SameDayInterval.start, SameDayInterval.end]
+      .map(this.timeMapper.dayTimeToString.bind(this.timeMapper))
+      .join(TIME_DIVIDER);
+
+    return `${SameDayInterval.dayNumber}T${timePart}`;
   }
 
   public mapFromString(wholeIntervalString: string): SameDayInterval {
@@ -52,7 +54,7 @@ export class SameDayIntervalMapper {
       wholeIntervalString,
     );
 
-    const numberOfDay = this.validateNumberOfDay(this.parsePositiveIntStrict(dayNumberString));
+    const numberOfDay = this.timeMapper.dayNumberFromString(dayNumberString);
 
     const [startString, endString] = this.validateParts(
       timesPart.split(TIME_DIVIDER),
@@ -61,47 +63,14 @@ export class SameDayIntervalMapper {
 
     return {
       dayNumber: numberOfDay,
-      start: this.convertToTime(startString),
-      end: this.convertToTime(endString),
+      start: this.timeMapper.dayTimeFromString(startString),
+      end: this.timeMapper.dayTimeFromString(endString),
     };
-  }
-
-  private withLeading0(number: Number): string {
-    return number.toString(10).padStart(2, '0');
-  }
-
-  private timeToString({ hour, minute }: DayTime): string {
-    return this.withLeading0(hour) + ':' + this.withLeading0(minute);
-  }
-
-  private parsePositiveIntStrict(value: string, allowLeading0 = false): number {
-    const valueToCheck = allowLeading0 && value.startsWith('0') ? value.slice(1) : value;
-
-    if (!/^\d+$/.test(valueToCheck)) {
-      throw new Error(`Invalid integer: "${valueToCheck}"`);
-    }
-
-    return Number(valueToCheck);
   }
 
   private validateParts(parts: string[], wholeIntervalString: string): [string, string] {
     if (parts.length !== 2)
       throw new Error('Invalid string format for interval: ' + wholeIntervalString);
     return parts as [string, string];
-  }
-
-  private validateNumberOfDay(rawDayNumber: number): DayNumber {
-    if (rawDayNumber < 1 || rawDayNumber > 7) throw new Error('Invalid DayNumber: ' + rawDayNumber);
-    return rawDayNumber as DayNumber;
-  }
-
-  private convertToTime(timeString: string): DayTime {
-    const result = timeString.match(TIME_REGEX);
-    if (result?.length !== 3) throw new Error('Invalid time string: ' + timeString);
-
-    return {
-      hour: this.parsePositiveIntStrict(result[1], true) as Hour,
-      minute: this.parsePositiveIntStrict(result[2], true) as Minute,
-    };
   }
 }
