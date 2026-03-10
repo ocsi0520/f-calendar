5d47217f2ba061461196d1e3e081002d8f4c4889
Faragó Tamás <ocsi0520@gmail.com>
2026-03-10 09:37:12 +0100

    use ScheduleGenerator

--- src/tabs/app-generate-week-schedule/app-generate-week-schedule.ts
+++ src/tabs/app-generate-week-schedule/app-generate-week-schedule.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { AppCalendar } from '../../time-management/calendar/app-calendar/app-calendar';
 import { Session } from '../../time-management/session';
-// import { TableGenerator } from '../../time/ScheduleGenerator/utils/TableGenerator';
-// import { Table } from '../../time/ScheduleGenerator/Table';
-// import { ScheduleGenerator } from '../../time/ScheduleGenerator/ScheduleGenerator';
+import { ScheduleGenerator } from '../../time-management/ScheduleGenerator/ScheduleGenerator';
+import { TableGenerator } from '../../time-management/ScheduleGenerator/table-generator/TableGenerator';
+import { Table } from '../../time-management/ScheduleGenerator/Table';
 
-type Table = any;
+const tableGenerationLabel = 'table generation';
 
 @Component({
   selector: 'app-generate-week-schedule',
@@ -15,20 +15,25 @@ type Table = any;
   styleUrl: './app-generate-week-schedule.scss',
 })
 export class AppGenerateWeekSchedule {
-  // private tableGenerator = inject(TableGenerator);
-  // private scheduleGenerator = inject(ScheduleGenerator);
+  private tableGenerator = inject(TableGenerator);
+  private scheduleGenerator = inject(ScheduleGenerator);
   public generatedData = signal<{ table: Table; schedule: Array<Session> } | null>(null);
+
   public generateSchedule(): void {
-    let newTable: Table | undefined;
+    const newTable = this.generateNewTable();
+    if (newTable) this.generateNextIterationWith(newTable);
+  }
+
+  private generateNewTable(): Table | undefined {
     try {
-      // newTable = this.tableGenerator.generateTable();
-      newTable = {};
+      return this.tableGenerator.generateTable();
     } catch (e) {
       // TODO: snackbar and avoid next generation
       alert('error at generation: ' + (e as Error).message);
+      return undefined;
     }
-    if (newTable) this.generateNextIterationWith(newTable);
   }
+
   public generateNextIteration(): void {
     const previousTable = this.generatedData()?.table;
     if (!previousTable) return;
@@ -36,29 +41,22 @@ export class AppGenerateWeekSchedule {
   }
 
   private generateNextIterationWith(table: Table): void {
-    // TODO: finish
     console.log('generate w/', table);
-    //   const prevDate = new Date();
-    //   console.log('starting at', prevDate.toISOString());
-    //   let finalDate!: Date;
-    //   this.generatedData.set(null);
-    //   try {
-    //     // TODO: do the generation on a separate thread/worker or whatever
-    //     // OR just make a simple setTimeout, so that the UI can show some loading
-    //     const schedule = this.scheduleGenerator.generateScheduleFrom(table);
-    //     this.generatedData.set({
-    //       table: table,
-    //       schedule,
-    //     });
-    //     finalDate = new Date();
-    //   } catch (e) {
-    //     finalDate = new Date();
-    //     // TODO: snackbar and avoid next generation
-    //     alert('error: ' + (e as Error).message);
-    //   } finally {
-    //     console.log('prevDate.toISOString()', prevDate.toISOString());
-    //     console.log('finalDate.toISOString()', finalDate.toISOString());
-    //     console.log('difference in seconds:', (finalDate.valueOf() - prevDate.valueOf()) / 1_000);
-    //   }
+    console.time(tableGenerationLabel);
+    this.generatedData.set(null);
+    try {
+      // TODO: do the generation on a separate thread/worker or whatever
+      // OR just make a simple setTimeout, so that the UI can show some loading
+      const schedule = this.scheduleGenerator.generateScheduleFrom(table);
+      this.generatedData.set({
+        table: table,
+        schedule,
+      });
+      console.timeEnd(tableGenerationLabel);
+    } catch (e) {
+      console.timeEnd(tableGenerationLabel);
+      // TODO: snackbar and avoid next generation
+      alert('error: ' + (e as Error).message);
+    }
   }
 }
