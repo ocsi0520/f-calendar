0c20786aed4976e9b0d9b4df1f6518e361128199
Faragó Tamás <ocsi0520@gmail.com>
2026-03-16 17:55:26 +0100

    add getOccupiedCellsForDay to TableUtils

--- src/time-management/ScheduleGenerator/TableManager/TableUtils.ts
+++ src/time-management/ScheduleGenerator/TableManager/TableUtils.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ClientInfo, Table, TableCell } from '../Table';
+import { DayNumber } from '../../definition/time-components';
 
 @Injectable({ providedIn: 'root' })
 export class TableUtils {
@@ -30,5 +31,17 @@ export class TableUtils {
   public getCellAt(table: Table, clientInfo: ClientInfo, possibleCellIndex: number): TableCell {
     const actualIndex = clientInfo.possibleCellIndexes[possibleCellIndex];
     return table.cellPart.views.linear[actualIndex];
+  }
+
+  public getOccupiedCellsForDay(table: Table, dayNumber: DayNumber): Array<TableCell> {
+    const byDayView = table.cellPart.views.byDay;
+    const allCells = table.cellPart.views.linear;
+    const startIndex = dayNumber === 1 ? 0 : byDayView[dayNumber - 2] + 1;
+    const endIndex = dayNumber === 7 ? allCells.length - 1 : byDayView[dayNumber - 1];
+    const result: Array<TableCell> = [];
+    for (let i = startIndex; i <= endIndex; i++)
+      if (allCells[i].clientIdsInvolved.length > 0) result.push(allCells[i]);
+
+    return result;
   }
 }
