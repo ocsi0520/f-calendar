706e15d776fee8441c4a8421d91a0989e1fd3940
Faragó Tamás <ocsi0520@gmail.com>
2026-03-16 20:10:49 +0100

    integrate AllowedSpecificationsService

--- src/time-management/ScheduleGenerator/SpecificationManagerFactory.ts
+++ src/time-management/ScheduleGenerator/SpecificationManagerFactory.ts
@@ -11,6 +11,8 @@ import { SpecificationManager } from './specification/SpecificationManager';
 import { SameDayIntervalManager } from '../managers/SameDayIntervalManager';
 import { TimeMapper } from '../mappers/TimeMapper';
 import { TableUtils } from './TableManager/TableUtils';
+import { NoFollowingDayFor2SessionsSpecification } from './specification/rules/NoFollowingDayFor2SessionsSpecification';
+import { AllowedSpecificationsService } from './specification/allowance/AllowedSpecifications.service';
 
 @Injectable({ providedIn: 'root' })
 export class SpecificationManagerFactory {
@@ -20,26 +22,35 @@ export class SpecificationManagerFactory {
     private sameDayIntervalManager: SameDayIntervalManager,
     private pairService: ClientPairService,
     private tableUtils: TableUtils,
+    private allowedSpecs: AllowedSpecificationsService,
   ) {}
 
   private getAllSpecifications(): Array<ScheduleSpecification> {
+    const { breakfast, lunch, noFollowingDayFor2Sessions } = this.allowedSpecs.load();
     const morningChecker = new MorningChecker();
     return [
       new ProperPairsSpecification(this.pairService),
       new NoOverlappingSessionsSpecification(this.sameDayIntervalManager, this.tableUtils),
-      new BreakfastSpecification(
-        this.sameDayIntervalManager,
-        this.timeManager,
-        morningChecker,
-        this.tableUtils,
-      ),
-      new LunchSpecification(
-        morningChecker,
-        this.sameDayIntervalManager,
-        this.timeManager,
-        this.tableUtils,
-      ),
-    ];
+      noFollowingDayFor2Sessions
+        ? new NoFollowingDayFor2SessionsSpecification(this.tableUtils)
+        : null,
+      breakfast
+        ? new BreakfastSpecification(
+            this.sameDayIntervalManager,
+            this.timeManager,
+            morningChecker,
+            this.tableUtils,
+          )
+        : null,
+      lunch
+        ? new LunchSpecification(
+            morningChecker,
+            this.sameDayIntervalManager,
+            this.timeManager,
+            this.tableUtils,
+          )
+        : null,
+    ].filter((rule) => rule !== null);
   }
 
   public create(): SpecificationManager {
--- src/time-management/ScheduleGenerator/__tests__/ScheduleGenerator.spec.ts
+++ src/time-management/ScheduleGenerator/__tests__/ScheduleGenerator.spec.ts
@@ -4,16 +4,23 @@ import { ScheduleGenerator } from '../ScheduleGenerator';
 import { outputTable } from './output-table';
 import { TableGenerator } from '../table-generator/TableGenerator';
 import { testClients, testMyTime, testNextId, testPairs } from './test-input';
+import { AllowedSpecificationsDescriptor } from '../specification/allowance/AllowedSpecificationsDescriptor';
 
 describe(ScheduleGenerator.name, () => {
   let tableGenerator: TableGenerator;
   let unitUnderTest: ScheduleGenerator;
 
   beforeAll(() => {
+    const allowedSpecs: AllowedSpecificationsDescriptor = {
+      breakfast: true,
+      lunch: true,
+      noFollowingDayFor2Sessions: false,
+    };
     localStorage.setItem('calendar_clients', JSON.stringify(testClients));
     localStorage.setItem('calendar_clients_id', JSON.stringify(testNextId));
     localStorage.setItem('client_pairs', JSON.stringify(testPairs));
     localStorage.setItem('my_time', JSON.stringify(testMyTime));
+    localStorage.setItem('allowed_specs', JSON.stringify(allowedSpecs));
   });
 
   beforeEach(() => {
